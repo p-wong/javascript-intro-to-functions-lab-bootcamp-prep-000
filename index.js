98df8bfd4c46d20244ca535326d75f39d14b4853
Patricia Wong <patriciawong08@gmail.com>
2017-11-17 22:27:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,11 +20,11 @@ function sayHiToGrandma(string) {
   var lowercase = 'hello!'
   var mixedCase = 'Hi there!'
   
-  if uppercase.toUpperCase() === uppercase {
+  if (uppercase.toUpperCase() === uppercase) {
     
     return "YES INDEED!"
   
-  } else if lowercase.toLowerCase() === lowercase {
+  } else if (lowercase.toLowerCase() === lowercase) {
     
     return "I can't hear you!"
     
