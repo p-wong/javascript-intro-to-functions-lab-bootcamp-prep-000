e82c09d75c00d2bb941a714ec7b4822ff1e91f46
Patricia Wong <patriciawong08@gmail.com>
2017-11-17 22:18:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ function shout(string) {
   return string.toUpperCase()
 }
 
-function whisper(strnig) {
+function whisper(string) {
   return string.toLowerCase()
 }
 
+function logshout(string) {
+  console.log(string)
+}
